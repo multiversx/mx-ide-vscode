c0ae60ec80240d2d16336d613938c8595561bdbc
Andrei Băncioiu <andrei.bancioiu@elrond.com>
2019-10-20 09:57:04 +0300

    Separate files for view templates.

--- src/mainView.ts
+++ src/mainView.ts
@@ -121,6 +121,8 @@ export class MainView {
         let html: string = FsFacade.readFileInContent("mainView.html");
         let baseHref = this.getBaseHref();
         html = html.replace("{{baseHref}}", baseHref.toString());
+        html = html.replace("{{template.smartContractPanel.html}}", FsFacade.readFileInContent("template.smartContractPanel.html"));
+        html = html.replace("{{template.vmOutput.html}}", FsFacade.readFileInContent("template.vmOutput.html"));
         return html;
     }
 
