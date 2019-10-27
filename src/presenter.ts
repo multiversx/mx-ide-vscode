705168ca4ad9a8e05cd1414766baf8d51d5649ce
Andrei Băncioiu <andrei.bancioiu@elrond.com>
2019-10-27 10:38:16 +0200

    Cleanup - remove not needed code.

--- src/presenter.ts
+++ src/presenter.ts
@@ -1,6 +1,4 @@
 import * as vscode from 'vscode';
-import { FsFacade } from './utils';
-import { asyncErrorCatcher } from './errors';
 import { MainView as MainView } from './mainView';
 
 export class Presenter {
@@ -14,12 +12,6 @@ export class Presenter {
         vscode.window.showErrorMessage(message);
     }
 
-    public static displayContentInNewTab(content: string) {
-        let tempFile = FsFacade.createTempFile("simple_output.txt", content);
-        let uri = vscode.Uri.file(tempFile);
-        vscode.window.showTextDocument(uri);
-    }
-
     public static getActiveFilePath() {
         let activeTextEditor = vscode.window.activeTextEditor;
 
@@ -29,17 +21,6 @@ export class Presenter {
 
         let path = activeTextEditor.document.uri.fsPath;
         return path;
-    }
-
-    public static askSimpleInput(options: any) {
-        let inputBoxOptions: vscode.InputBoxOptions = {
-            value: options.placeholder,
-            prompt: options.title
-        };
-
-        vscode.window.showInputBox(inputBoxOptions)
-            .then(options.onInput, asyncErrorCatcher)
-            .then(() => { }, asyncErrorCatcher);
     }
 
     public static showMainView() {
