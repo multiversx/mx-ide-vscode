cb1499ee745270cb5fbc03556a22fd218af24167
miiu96 <mihai9625@gmail.com>
2021-03-09 19:14:57 +0200

    add empty lines

--- src/utils.spec.ts
+++
@@ -1,9 +0,0 @@
-import {assert} from "chai";
-import { isLocalVersionOk } from "./utils";
-
-describe("test compare local version with min version", () => {
-    it("should return true", () => {
-        let isOk = isLocalVersionOk('erdpy 1.0.2', '1.0.1')
-        assert.equal(true, isOk)
-    });
-});
