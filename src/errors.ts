598c69b64b0a30beeda8919cef9ce9f681f76492
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-04-12 13:48:04 +0300

    Fix typo.

--- src/errors.ts
+++ src/errors.ts
@@ -3,7 +3,7 @@ import { Feedback } from "./feedback";
 /**
  * This function should be called when a top-level error occurs.
  * Examples of top-level errors are:
- * - errors that occur as a result of a executing a command of the extension
+ * - errors that occur as a result of executing a command of the extension;
  * - errors that occur on a call initiated by VSCode's view rendering logic (e.g. errors when preparing data for tree views).
  */
 export async function onTopLevelError(error: any): Promise<void> {
